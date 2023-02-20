517fc624e341bb7978a77068f4513e2deaf56db8
S_Aota <ssssaota@gmail.com>
2023-02-21 00:30:03 +0900

    ライブラリ化

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tolplotlib',
+    version='0.0.0',
+
+    author='SeijiAota',
+    author_email='aota-seiji493@g.ecc.u-tokyo.ac.jp',
+
+    packages=['tolplotlib']
+)
